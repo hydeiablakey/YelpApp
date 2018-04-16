d92efb2edeccb563831dd8c747a41c92e4629eea
Hydeia <blakeyhydeia@gmail.com>
2018-04-16 12:02:26 -0400

    Add Search Result item, display price, and location

---
+++ components/presentational/SearchResultItem.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react'; 
+
+
+export default class SearchResultItem extends Component {
+	render() {
+		return (	
+			<div className="searchResultItem_container">
+			<p>It's an item</p>
+			</div>
+		);
+	}
+}
