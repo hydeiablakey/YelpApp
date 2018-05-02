a485f2525e699de9f3edb79b06c576bc9c84ce0d
Hydeia <blakeyhydeia@gmail.com>
2018-05-02 16:14:24 -0400

    Add line styling to search results

--- components/containers/SearchResults.js
+++ components/containers/SearchResults.js
@@ -4,21 +4,20 @@ import SearchResultButtons from '../presentational/SearchResultButtons';
 
 export default class SearchResults extends Component {
 
+//work on getting a map element to display on click for a search result.
 	_getColor = ( rating ) => {
-		//some sort of logic here that spits out a color
-		// this will generate a hed value
+
 
 		if ( rating > 0 && rating < 2 ) {
 			return '#F0152D';
-
 		} else if (rating >=2 && rating < 3) {
 			return '#F08E2D'
 		} else if (rating >=3 && rating < 4 ) {
 			return '#F0B52D'
 		} else if (rating >=4 &&  rating < 5 ) {
-			return '#6AA82A'
+			return '#A0B62A'
 		} else {
-			return '##1EA82A'
+			return '#1EA82A'
 		}
 
 	}
@@ -28,15 +27,10 @@ export default class SearchResults extends Component {
 
 		const listItems = response.map(( item ) => {
 			const rating = item.rating;
-			console.log(rating);
-
-			// if (rating > 0 && rating <= 1 ) {
-
-			// }
-
+			//console.log(rating); debugging
 			return (
 				<li className="searchItem" key={ `Sumimasen_${ Math.random() * (new Date()) }` }> 
-					<span className="searchName">{ item.name }</span>
+					<span  className="searchName">{ item.name }</span>
 					<span className="rating" style={{ color: this._getColor( item.rating )}}>{ item.rating }</span>
 				</li>
 			)
@@ -63,7 +57,7 @@ export default class SearchResults extends Component {
 				<p>Welcome to a default search message. </p>
 					<hr width="50%" />
 				<div className="prompt">
-					<p className="prompt_msg">This is a box full of information. Use it wisely!</p>
+					<p className="prompt_msg">Box of information. Use it wisely!</p>
 				</div>
 			</div>
 		)
